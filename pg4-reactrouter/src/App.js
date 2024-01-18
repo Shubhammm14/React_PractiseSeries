d670b2435e8d081a4d6dd289021d9c50b5afb116
shubhammm14 <shubhamshaw905@gmail.com>
2024-01-19 03:24:23 +0530

    fourth commit

---
+++ pg4-reactrouter/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Component/Home';
+import Footer from './Component/Footer';
+import Header from './Component/Header';
+
+function App() {
+  return (
+  <div>
+    <Header/>
+    <Home/>
+    <Footer/>
+  </div>
+  );
+}
+
+export default App;
