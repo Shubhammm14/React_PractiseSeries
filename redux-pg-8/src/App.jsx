607e9f8413a7dd8ca54ad0e9701736d07762dc50
shubhammm14 <shubhamshaw905@gmail.com>
2024-01-24 00:14:41 +0530

    projjo commit

---
+++ redux-pg-8/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import AddTodo from './Components/AddTodos'
+import Todos from './Components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
